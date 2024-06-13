af175a9a320f3662b0bc5c84c35b8a60d9bec8ce
Solyn <git@solyn.xyz>
2024-06-13 11:42:42 +0100

    🌱 Adiciona analíticas da Vercel e outros modules
    Adicionei o SpeedInsights da Vercel para analíticas e também adicionei alguns modules do Nuxt😁

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <template>
   <AppLayout>
+    <SpeedInsights />
     <NuxtPage />
   </AppLayout>
 </template>
 
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/nuxt';
+</script>
 <style>
 html {
   font-family: $dt('font.sans');
